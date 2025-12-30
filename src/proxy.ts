aa3197c9a65f3f414aba914f3be3167ed32af933
Lean <leian1306@gmail.com>
2025-12-30 09:01:49 +0100

    document proxy paths

--- src/proxy.ts
+++ src/proxy.ts
@@ -10,16 +10,17 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - signin
-     * - houston
-     * - auth callback urls
-     * - terms & conditions and privacy policy
-     * - manifest
-     * - sw.js
+     * - signin (signin page)
+     * - houston (admin panel page)
+     * - auth callback urls (oauth process)
+     * - terms (terms & conditions page)
+     * - privacy (privacy policy page)
+     * - manifest (PWA manifest)
+     * - sw.js (PWA Service Worker)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - images
+     * - images (any image)
      */
     '/((?!signin|houston|auth|terms|privacy|manifest|sw.js|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
   ],
