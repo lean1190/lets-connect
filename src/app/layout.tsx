d8a4f12c49c27955848bc91b4b0cf82ec90e82b5
Lean <leian1306@gmail.com>
2026-01-10 19:46:14 -0300

    layout: adjust bar style in pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
   },
   appleWebApp: {
     capable: true,
-    statusBarStyle: 'black-translucent',
+    statusBarStyle: 'default',
     title: "Let's connect",
     startupImage: [
       {
